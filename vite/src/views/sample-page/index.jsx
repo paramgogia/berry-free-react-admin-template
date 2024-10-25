60bb4371c5e698259b9f54373565916a423450bf
paramgogia <paramgogia25@gmail.com>
2024-10-25 21:54:23 +0530

    first param

--- vite/src/views/sample-page/index.jsx
+++ vite/src/views/sample-page/index.jsx
@@ -3,17 +3,12 @@ import Typography from '@mui/material/Typography';
 
 // project imports
 import MainCard from 'ui-component/cards/MainCard';
-
+import ProductTable from 'views/dashboard/harshit/ProductTable';
 // ==============================|| SAMPLE PAGE ||============================== //
 
 const SamplePage = () => (
   <MainCard title="Sample Card">
-    <Typography variant="body2">
-      Lorem ipsum dolor sit amen, consenter nipissing eli, sed do elusion tempos incident ut laborers et doolie magna alissa. Ut enif ad
-      minim venice, quin nostrum exercitation illampu laborings nisi ut liquid ex ea commons construal. Duos aube grue dolor in reprehended
-      in voltage veil esse colum doolie eu fujian bulla parian. Exceptive sin ocean cuspidate non president, sunk in culpa qui officiate
-      descent molls anim id est labours.
-    </Typography>
+    <ProductTable />
   </MainCard>
 );
 
